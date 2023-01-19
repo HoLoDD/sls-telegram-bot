e350160d50331ff68ce2e0d2fa07c1bfa82539bd
HoLoDDD <mykyta.kozlov@chatbotsstudio.com>
2023-01-19 23:04:09 +0100

    init

---
+++ index.js
@@ -0,0 +1,58 @@
+const mode = process.argv.slice(2)[0] || 'prod'; // 'dev' || 'prod'
+const TelegramBot = require('node-telegram-bot-api');
+const { configuration } = require('./config.js');
+const config = configuration[mode]; // dynamically load config
+const bot = new TelegramBot(config.token, { polling: mode === 'dev' });
+
+/**
+ * MESSAGE HANDLER
+ */
+handleMessage = (message) => {
+    // Your code here!
+    if (message.text) {
+        // Echos the received message
+        bot.sendMessage(message.chat.id, `Your chatId:\n"${message.chat.id}"`);
+    }
+};
+
+/**
+ * EVENT HANDLERS
+ */
+if (bot.options.polling) {
+    // Polling event handlers
+    bot.on('message', (message) => {
+        handleMessage(message);
+    });
+}
+
+exports.handler = (event, context, callback) => {
+    // Responding "200 - OK" to telegram servers
+    callback(null, {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify({ ok: true }),
+    });
+    // Extracting message from JSON
+    const { body } = event;
+    handleMessage(JSON.parse(body).message);
+};
+
+exports.sendMessage = (event, context, callback) => {
+    // Responding "200 - OK" to telegram servers
+    callback(null, {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify({ ok: true }),
+    });
+    // Extracting message from JSON
+    const { body } = event;
+    const { message, chatId } = JSON.parse(body);
+
+    bot.sendMessage(chatId, message);
+};
